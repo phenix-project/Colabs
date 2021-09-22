265613191bce8351fd57f8e440b7d9dd621691e3
terwill <tterwilliger@newmexicoconsortium.org>
2021-09-21 21:20:11 -0600

    Turn off templates if not found (fix typo)

--- alphafold2/edit_AlphaFold2_with_Manual_Template.py
+++ alphafold2/edit_AlphaFold2_with_Manual_Template.py
@@ -113,7 +113,7 @@ replacement_groups.append(get_replacement_group(
  '''        "    template_features = mk_mock_template(query_sequence * homooligomer)\\n",'''],
  ['''        "  if template_paths is None:\\n",''',
  '''        "    template_features = mk_mock_template(query_sequence * homooligomer)\\n",''',
-  '''        "    use_templates = False\\n''']))
+  '''        "    use_templates = False\\n"''']))
 
 for rg in replacement_groups:
   print(rg)
