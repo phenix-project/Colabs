33b868e044d806123ba2826a08c4f63815bc2f60
terwill <tterwilliger@newmexicoconsortium.org>
2022-02-02 08:01:59 -0700

    fix typo

--- alphafold2/phenix_colab_utils.py
+++ alphafold2/phenix_colab_utils.py
@@ -151,7 +151,7 @@ def install_phenix(password = None, version = None):
       raise AssertionError("Unable to download...please check your Phenix version and password?")
 
     # Move files to working directory
-    shell("mv phenix-online.org/download/installers/$version/linux-64/* .")
+    shell("mv phenix-online.org/download/installers/%s/linux-64/* ." %(version))
     shell("rm -fr phenix-online.org")
     shell("touch PHENIX_DOWNLOADED")
     print("Phenix has been downloaded.")
