a336ac96718b5593c1f7884376edc5e040f5a64e
terwill <tterwilliger@newmexicoconsortium.org>
2022-02-02 07:34:24 -0700

    write to sh file, not edit

--- alphafold2/phenix_colab_utils.py
+++ alphafold2/phenix_colab_utils.py
@@ -116,7 +116,7 @@ def run_fix_paths():
     f = open(
      "/usr/local/lib/python3.7/site-packages/phenix/command_line/pulchra.sh",
      'w')
-    print("""exec /usr/local/share/cctbx/pulchra/exe/pulchra "$@" """),
+    print("""exec /usr/local/share/cctbx/pulchra/exe/pulchra "$@" """,
       file = f
     f.close()
 
